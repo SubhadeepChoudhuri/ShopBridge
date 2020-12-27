dd390e9c7b232c2d0649672c349f8faf0b2cc8dd
SubhadeepChoudhuri <36217402+SubhadeepChoudhuri@users.noreply.github.com>
2020-12-27 18:38:43 +0530

    Update server.js

--- src/app/server/server.js
+++ src/app/server/server.js
@@ -8,18 +8,84 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+
+
 app.get('/', function(req,res){
-    res.send('Hello from server!');
+    //res.send('Hello from server!');
     //console.log(res);
+    var sql = require("mssql");
+    // config for your database
+    var config = {
+        user: 'Subho',
+        password: '123',
+        server: 'localhost\\SQLEXPRESS', 
+        database: 'Subho_DB',
+        options: {           
+            encrypt: true,
+            enableArithAbort: true
+        }
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select itemID,name,descr,price,photo from [Subho_DB].[dbo].[ItemInfo]', function (err, recordset) {
+            
+            if (err) console.log(err);
+            // send records as a response
+            res.send(recordset);
+            
+        });
+});
+
 });
 
 app.post('/postData', function(req,res){
+
+    console.log(666);
+
     console.log(req.body);
-    res.status(200).send({'message':'Data received!'});
-    /*if(res.status == 200){
-        console.log(666);
-        console.log(res);   
-    }*/   
+
+    var sql = require("mssql");
+
+   // config for your database
+   var config = {
+    user: 'Subho',
+    password: '123',
+    server: 'localhost\\SQLEXPRESS', 
+    database: 'Subho_DB',
+    options: {
+            encrypt: true,
+            enableArithAbort: true
+     }
+};
+
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query("INSERT INTO [Subho_DB].[dbo].[ItemInfo] (name, descr, price, photo) VALUES ('"+req.body.name+"', '"+req.body.descr+"', '"+req.body.price+"', '"+req.body.photo+"')", function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset); 
+            
+        });
+});  
+    
 });
 
 app.listen(PORT, function(){
