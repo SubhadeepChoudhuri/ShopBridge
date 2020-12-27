39822e0983ad8c26f88acc227d8df334d876a732
SubhadeepChoudhuri <36217402+SubhadeepChoudhuri@users.noreply.github.com>
2020-12-27 18:39:46 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddItemComponent } from './add-item/add-item.component';
-import { ItemDetailsComponent } from './item-details/item-details.component';
 
-const routes = [
-  { path: 'itemDetails', component: ItemDetailsComponent } 
-];
+const routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { enableTracing: true })],
